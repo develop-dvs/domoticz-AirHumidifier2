0e335ba9309ef2b84a4e28c5bdeb83d8f7a2ec1b
Develop <develop@divasoft.ru>
2022-01-06 22:11:00 +0300

    clear

--- debug.py
+++
@@ -1,6 +0,0 @@
-import miio.airhumidifier
-import miio.airhumidifier_miot
-
-MyHumidifier = miio.airhumidifier.AirHumidifier("192.168.1.124", "22be42754b31ac440d6394254b38f71c")
-data = MyHumidifier.status()
-print(data)
